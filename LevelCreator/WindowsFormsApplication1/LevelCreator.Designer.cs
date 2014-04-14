ae01cfecd170499f953d888d0e8d9187bee482d5
bwd39 <blairwdoherty@gmail.com>
2014-04-15 11:45:44 +1200

    LevelCreator

---
+++ LevelCreator/WindowsFormsApplication1/LevelCreator.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class LevelCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exiting = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exiting
+            // 
+            this.exiting.Location = new System.Drawing.Point(1071, 443);
+            this.exiting.Name = "exiting";
+            this.exiting.Size = new System.Drawing.Size(112, 118);
+            this.exiting.TabIndex = 0;
+            this.exiting.Text = "Exit";
+            this.exiting.UseVisualStyleBackColor = true;
+            this.exiting.Click += new System.EventHandler(this.button1_Click);
+            this.exiting.MouseClick += new System.Windows.Forms.MouseEventHandler(this.exiting_MouseClick);
+            // 
+            // LevelCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 986);
+            this.Controls.Add(this.exiting);
+            this.Name = "LevelCreator";
+            this.Text = "Theseus vs. Minotaur Level Creator";
+            this.Load += new System.EventHandler(this.LevelCreator_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exiting;
+    }
+}
+
---
+++ LevelCreator/WindowsFormsApplication1/LevelCreator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class LevelCreator : Form
+    {
+        public LevelCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void LevelCreator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            
+            
+            Application.Exit();
+        }
+
+        private void LevelCreator_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show("Do you really wanna exit ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+
+        } 
+
+        private void exiting_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+    }
+}
